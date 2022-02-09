f07382c2f1415300b92cdc23e61d02b8cb53964f
Mongruel <arduinni@gmail.com>
2022-02-09 06:18:27 -0300

    Componente botão 2

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
-    <button className='button'>{props.label}</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.triple ? 'triple' : ''
+    classes += props.double ? 'double' : ''
+
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+    
+}
+    
